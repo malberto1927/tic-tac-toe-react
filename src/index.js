f85c69003940f5c71848aff40bbbc9162f59c207
Mario Alberto Villagrana Hurtado <malberto1927@gmail.com>
2019-04-04 18:14:28 -0600

    se esta trabajando en lo states.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            squares: Array(9).fill(null),
+        }
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice()
+        squares[i] = 'X'
+        this.setState({squares: squares})
+    }
+
     renderSquare(i) {
-        return <Square value={i} />
+        return (
+            <Square 
+                value={this.state.squares[i]} 
+                onClick={() => this.handleClick(i)}
+            />
+        )
     }
 
     render () {
@@ -34,9 +52,21 @@ class Board extends React.Component {
 }
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: null,
+        }
+    }
+
     render () {
         return (
-            <button className="square">{ this.props.value }</button>
+            <button 
+                className="square" 
+                onClick={() => this.props.onClick()}
+            >
+                { this.props.value }
+            </button>
         )
     }
 }
